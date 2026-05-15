8c56b2a58aedfe463ec4506bf71dcc00aebfa4a6
Jeremiah <jeremiaholuwafemi16@gmail.com>
2026-05-15 04:48:54 +0100

    extra screens for admin web

--- lib/core/features/admin/ui/dashboard_screen.dart
+++ lib/core/features/admin/ui/dashboard_screen.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pro_app/core/features/admin/providers/dashboard_providers.dart';
+import 'package:pro_app/core/features/admin/ui/widgets/admin_stat_card.dart';
+import 'package:pro_app/core/theme/app_theme.dart';
 
-class AdminDashboardScreen extends StatelessWidget {
+class AdminDashboardScreen extends ConsumerWidget {
   const AdminDashboardScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Admin Dashboard Screen')));
+  Widget build(BuildContext context, WidgetRef ref) {
+    final appColors = context.appColors;
+
+    // Now the UI only watches one provider for all the stats!
+    final stats = ref.watch(dashboardStatsProvider);
+
+    return Scaffold(
+      backgroundColor: appColors.background,
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            GridView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 300,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                mainAxisExtent: 180,
+              ),
+              itemCount: stats.length,
+              itemBuilder: (context, index) =>
+                  AdminStatCard(model: stats[index]),
+            ),
+
+            const SizedBox(height: 40),
+            // More dashboard content can go here
+          ],
+        ),
+      ),
+    );
   }
 }
